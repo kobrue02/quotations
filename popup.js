fe3bf282618755cac4e4d18727e236c6527ec320
Konrad Brüggemann <konrad.brueggemann@uni-potsdam.de>
2023-10-22 16:35:22 +0200

    added button look

--- popup.js
+++ popup.js
@@ -3,8 +3,25 @@ const symbols = document.querySelectorAll('.symbol');
 symbols.forEach(symbol => {
     symbol.addEventListener('click', () => {
         const textToCopy = symbol.getAttribute('data-text');
+        const activeElement = document.activeElement;
 
-        navigator.clipboard.writeText(textToCopy)
+        if (activeElement && (activeElement.tagName === 'TEXTAREA')) {
+            // If the active element is an input field or a textarea, insert the symbol at the cursor position
+            const start = activeElement.selectionStart;
+            const end = activeElement.selectionEnd;
+            const currentValue = activeElement.value;
+
+            const newValue = currentValue.substring(0, start) + textToInsert + currentValue.substring(end);
+            activeElement.value = newValue;
+            activeElement.setSelectionRange(start + textToInsert.length, start + textToInsert.length);
+
+        }
+
+        else {
+            navigator.clipboard.writeText(textToCopy)
+        }
+
+        
 
     });
 });
